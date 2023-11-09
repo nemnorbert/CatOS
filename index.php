6f09857c8cea36f4b848fce03cba48891ae9b340
Norbert Nemeth <nth.norbert@gmail.com>
2023-11-09 15:48:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css?v=<?= time() ?>">
+    <title>CatOS</title>
+</head>
+<body>
+<div id="preLoader">
+    <div class="loading">
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="logo">
+            <div class="name">CatOS</div>
+            <div class="text">loading</div>
+        </div>
+    </div>
+    
+
+    <a class="poweredby" target="_blank" href="https://me.red-cat.hu">
+        <div>Powered by</div>
+        <img src="./img/logo_redcat.png" alt="Logo of Creator">
+    </a>
+</div>    
+
+</body>
+</html>
